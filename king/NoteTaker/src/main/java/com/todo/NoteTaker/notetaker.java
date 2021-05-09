140f751b50802dc12a513783b6c8d09a14bd64cd
surya4949 <sprathap049@gmail.com>
2021-05-09 15:20:05 +0530

    This is my first maven commit

---
+++ king/NoteTaker/src/main/java/com/todo/NoteTaker/notetaker.java
@@ -0,0 +1,10 @@
+package com.todo.NoteTaker;
+
+public class notetaker {
+
+	public static void main(String[] args) {
+		String String = "welcome to my first web app hosting";
+	    System.out.println(String);
+	}
+
+}
